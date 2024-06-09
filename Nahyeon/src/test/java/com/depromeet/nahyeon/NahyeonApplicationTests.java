6c4730144801bca13da9295d478121d96fba2ea7
nahyeon99 <nahyeonee99@gmail.com>
2024-06-10 02:53:17 +0900

    Nahyeon init

---
+++ Nahyeon/src/test/java/com/depromeet/nahyeon/NahyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.nahyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NahyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
