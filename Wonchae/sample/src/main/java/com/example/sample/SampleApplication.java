d2ba011aa39622db8827212071506a1f39083d59
ywonchae1 <ywonchae62@gmail.com>
2024-06-13 17:19:07 +0900

    feat: 계산기 기본 기능 구현

--- Wonchae/sample/src/main/java/com/example/sample/SampleApplication.java
+++ Wonchae/sample/src/main/java/com/example/sample/SampleApplication.java
@@ -3,11 +3,28 @@ package com.example.sample;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
+
 @SpringBootApplication
 public class SampleApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SampleApplication.class, args);
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter two numbers and an operator (e.g 1 + 2):");
+		String result = scanner.nextLine();
+		String[] parts = result.split(" ");
+		long num1 = Long.parseLong(parts[0]);
+		long num2 = Long.parseLong(parts[2]);
+		String operator = parts[1];
+		long answer = switch (operator) {
+            case "+" -> num1 + num2;
+            case "-" -> num1 - num2;
+            case "*" -> num1 * num2;
+            case "/" -> num1 / num2;
+            default -> throw new InvalidOperatorException();
+        };
+		System.out.println(answer);
+        //		SpringApplication.run(SampleApplication.class, args);
 	}
 
 }
