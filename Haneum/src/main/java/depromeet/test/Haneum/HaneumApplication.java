2eb0026787a425a8946b44ac52a3543c7da2af23
LeeHanEum <LeeHanEum160924@outlook.kr>
2024-06-14 23:13:11 +0900

    feat: initial commit

---
+++ Haneum/src/main/java/depromeet/test/Haneum/HaneumApplication.java
@@ -0,0 +1,13 @@
+package depromeet.test.Haneum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaneumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaneumApplication.class, args);
+	}
+
+}
