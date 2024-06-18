9b1abab054860c435ff24ed9c1eeb9c70d999174
nahyeon99 <nahyeonee99@gmail.com>
2024-06-18 12:05:32 +0900

    feat: 토이 프로젝트를 위한 초기 세팅

--- Nahyeon/src/main/java/com/depromeet/nahyeon/NahyeonApplication.java
+++ Nahyeon/src/main/java/com/depromeet/nahyeon/NahyeonApplication.java
@@ -1,18 +1,12 @@
 package com.depromeet.nahyeon;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class NahyeonApplication {
 
 	public static void main(String[] args) {
-		CalculationRequest calculationRequest = new CalculationRequestReader().read();
-
-		long answer = new Calculator().calculate(
-			calculationRequest.getNum1(),
-			calculationRequest.getNum2(),
-			calculationRequest.getOperator());
-
-		System.out.println(answer);
+		SpringApplication.run(NahyeonApplication.class, args);
 	}
 }
