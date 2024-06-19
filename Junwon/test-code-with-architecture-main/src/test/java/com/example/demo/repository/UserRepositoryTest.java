09e79f1951017ea69a27d575cdd41230a1422671
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-19 20:43:03 +0900

    (test) : 2주 2회차 실습 초반, findByIdAndStatus 메서드 동작 확인을 위한 테스트

--- Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/repository/UserRepositoryTest.java
+++ Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/repository/UserRepositoryTest.java
@@ -2,6 +2,8 @@ package com.example.demo.repository;
 
 import static org.assertj.core.api.AssertionsForClassTypes.*;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -29,6 +31,47 @@ public class UserRepositoryTest {
 
 		// then
 		assertThat(result.getId()).isNotNull();
+	}
+
+	@Test
+	void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다(){
+		// given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setEmail("study7823@gmail.com");
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("study7823");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+		// when
+		userRepository.save(userEntity);
+		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.ACTIVE);
+
+		// then
+		assertThat(result.isPresent()).isTrue();
+
+	}
+
+	@Test
+	void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다(){
+		// given
+		UserEntity userEntity = new UserEntity();
+		userEntity.setId(1L);
+		userEntity.setEmail("study7823@gmail.com");
+		userEntity.setAddress("Seoul");
+		userEntity.setNickname("study7823");
+		userEntity.setStatus(UserStatus.ACTIVE);
+		userEntity.setCertificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaaa");
+
+		// when
+		userRepository.save(userEntity);
+		Optional<UserEntity> result = userRepository.findByIdAndStatus(1, UserStatus.PENDING);
+
+		// then
+		assertThat(result.isEmpty()).isTrue();
+		assertThat(result.isPresent()).isFalse(); // 위와 동일
+
 	}
 
 }
