08f85b3bde6de741e17e75780f89cbab18c6e1e2
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-20 10:37:40 +0900

    refactor : 2주 2회차 service 리팩토링
    find는 Optional로 받아 null 반환 가능.
    get은 데이터가 없을 경우 Exception 반환.
    
    service 네이밍에서 의미를 내포하고 있으므로 메서드에서 post 등을 다시 언급할 필요가 없음.

--- Junwon/test-code-with-architecture-main/src/main/java/com/example/demo/service/UserService.java
+++ Junwon/test-code-with-architecture-main/src/main/java/com/example/demo/service/UserService.java
@@ -23,7 +23,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final JavaMailSender mailSender;
 
-    public Optional<UserEntity> getById(long id) {
+    public Optional<UserEntity> findById(long id) {
         return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE);
     }
 
@@ -32,13 +32,13 @@ public class UserService {
             .orElseThrow(() -> new ResourceNotFoundException("Users", email));
     }
 
-    public UserEntity getByIdOrElseThrow(long id) {
+    public UserEntity getById(long id) {
         return userRepository.findByIdAndStatus(id, UserStatus.ACTIVE)
             .orElseThrow(() -> new ResourceNotFoundException("Users", id));
     }
 
     @Transactional
-    public UserEntity createUser(UserCreateDto userCreateDto) {
+    public UserEntity create(UserCreateDto userCreateDto) {
         UserEntity userEntity = new UserEntity();
         userEntity.setEmail(userCreateDto.getEmail());
         userEntity.setNickname(userCreateDto.getNickname());
@@ -52,8 +52,8 @@ public class UserService {
     }
 
     @Transactional
-    public UserEntity updateUser(long id, UserUpdateDto userUpdateDto) {
-        UserEntity userEntity = getByIdOrElseThrow(id);
+    public UserEntity update(long id, UserUpdateDto userUpdateDto) {
+        UserEntity userEntity = getById(id);
         userEntity.setNickname(userUpdateDto.getNickname());
         userEntity.setAddress(userUpdateDto.getAddress());
         userEntity = userRepository.save(userEntity);
