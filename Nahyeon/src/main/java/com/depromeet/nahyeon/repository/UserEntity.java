fea62e3f2432752532b8daa268d1dbc95fa35f5c
nahyeon99 <nahyeonee99@gmail.com>
2024-06-20 15:14:00 +0900

    fix: h2 db 연동 오류 및 테스트 동시성 제어 실패 해결

--- Nahyeon/src/main/java/com/depromeet/nahyeon/repository/UserEntity.java
+++ Nahyeon/src/main/java/com/depromeet/nahyeon/repository/UserEntity.java
@@ -7,6 +7,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Getter;
@@ -19,7 +20,7 @@ import lombok.Setter;
 public class UserEntity {
 
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 
 	@Column(name = "email")
--- Nahyeon/src/test/java/com/depromeet/nahyeon/repository/UserRepositoryTest.java
+++ Nahyeon/src/test/java/com/depromeet/nahyeon/repository/UserRepositoryTest.java
@@ -5,47 +5,28 @@ import static org.assertj.core.api.Assertions.*;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.depromeet.nahyeon.model.UserStatus;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DataJpaTest(showSql = true)
+@TestPropertySource("classpath:test-application.yml")
+@Sql("/sql/user-repository-test-data.sql")
 public class UserRepositoryTest {
 
 	@Autowired
 	private UserRepository userRepository;
 
 	@Test
-	void UserRepository_가_제대로_연결되었다() {
-		// given
-		UserEntity userEntity = new UserEntity();
-		userEntity.setEmail("nahyeonee99@gmail.com");
-		userEntity.setAddress("Seoul");
-		userEntity.setNickname("nahyeonee99");
-		userEntity.setStatus(UserStatus.ACTIVE);
-		userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa-aaaaa");
-
-		// when
-		UserEntity result = userRepository.save(userEntity);
-
-		// then
-		assertThat(result.getId()).isNotNull();
-	}
-
-	@Test
 	void findByIdAndStatus_로_유저_데이터를_찾아올_수_있다() {
 		// given
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setEmail("nahyeonee99@gmail.com");
-		userEntity.setAddress("Seoul");
-		userEntity.setNickname("nahyeonee99");
-		userEntity.setStatus(UserStatus.ACTIVE);
-		userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa-aaaaa");
-
 		// when
-		userRepository.save(userEntity);
 		Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.ACTIVE);
 
 		// then
@@ -55,16 +36,7 @@ public class UserRepositoryTest {
 	@Test
 	void findByIdAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
 		// given
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setEmail("nahyeonee99@gmail.com");
-		userEntity.setAddress("Seoul");
-		userEntity.setNickname("nahyeonee99");
-		userEntity.setStatus(UserStatus.ACTIVE);
-		userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa-aaaaa");
-
 		// when
-		userRepository.save(userEntity);
 		Optional<UserEntity> result = userRepository.findByIdAndStatus(1L, UserStatus.PENDING);
 
 		// then
@@ -74,16 +46,7 @@ public class UserRepositoryTest {
 	@Test
 	void findByEmailAndStatus_로_유저_데이터를_찾아올_수_있다() {
 		// given
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setEmail("nahyeonee99@gmail.com");
-		userEntity.setAddress("Seoul");
-		userEntity.setNickname("nahyeonee99");
-		userEntity.setStatus(UserStatus.ACTIVE);
-		userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa-aaaaa");
-
 		// when
-		userRepository.save(userEntity);
 		Optional<UserEntity> result = userRepository.findByEmailAndStatus("nahyeonee99@gmail.com", UserStatus.ACTIVE);
 
 		// then
@@ -93,16 +56,7 @@ public class UserRepositoryTest {
 	@Test
 	void findByEmailAndStatus_는_데이터가_없으면_Optional_empty_를_내려준다() {
 		// given
-		UserEntity userEntity = new UserEntity();
-		userEntity.setId(1L);
-		userEntity.setEmail("nahyeonee99@gmail.com");
-		userEntity.setAddress("Seoul");
-		userEntity.setNickname("nahyeonee99");
-		userEntity.setStatus(UserStatus.ACTIVE);
-		userEntity.setCertificationCode("aaaaa-aaaaa-aaaaa-aaaaa");
-
 		// when
-		userRepository.save(userEntity);
 		Optional<UserEntity> result = userRepository.findByEmailAndStatus("nahyeonee99@gmail.com", UserStatus.PENDING);
 
 		// then
