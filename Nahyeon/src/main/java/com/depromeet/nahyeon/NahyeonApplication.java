92a7057b0c58edd3315abc08f1dec5b1aacdcfd2
nahyeon99 <nahyeonee99@gmail.com>
2024-06-21 13:40:27 +0900

    test: 이메일 인증 코드 불일치 케이스 테스트 코드 추가

--- Nahyeon/src/main/java/com/depromeet/nahyeon/NahyeonApplication.java
+++ Nahyeon/src/main/java/com/depromeet/nahyeon/NahyeonApplication.java
@@ -3,7 +3,15 @@ package com.depromeet.nahyeon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(
+	title = "Toy project",
+	version = "v1",
+	description = "테스트 코드를 추가하기 위한 연습에 사용될 토이 프로젝트입니다"
+))
 public class NahyeonApplication {
 
 	public static void main(String[] args) {
