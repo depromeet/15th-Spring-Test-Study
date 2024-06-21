a229170502590ce913f7d2cb9bd410127e7e6782
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-06-21 22:10:38 +0900

    test : 2주 4회차 UserControllerTest 부족한 커버리지 보완

--- Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/controller/UserControllerTest.java
+++ Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -74,6 +74,17 @@ public class UserControllerTest {
 	}
 
 	@Test
+	void 사용자는_인증_코드가_일치하지_않을_경우_권한_없음_에러를_내려준다() throws Exception {
+		// given
+		// when
+		// then
+		mockMvc.perform(
+				get("/api/users/2/verify")
+					.queryParam("certificationCode", "aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaac"))
+			.andExpect(status().isForbidden());
+	}
+
+	@Test
 	void 사용자는_내_정보를_불러올_때_개인정보인_주소도_갖고_올_수_있다() throws Exception {
 		// given
 		// when
