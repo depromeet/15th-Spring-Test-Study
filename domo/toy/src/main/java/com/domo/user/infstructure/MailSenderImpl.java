520412a418e0413242bb5835c864e07d64d7d26c
kdomo <dongho5088@naver.com>
2024-06-26 02:19:40 +0900

    refactor: MailSender 의존성 역전

---
+++ domo/toy/src/main/java/com/domo/user/infstructure/MailSenderImpl.java
@@ -0,0 +1,22 @@
+package com.domo.user.infstructure;
+
+import com.domo.user.service.port.MailSender;
+import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender {
+    private final JavaMailSender javaMailSender;
+
+    @Override
+    public void send(String email, String title, String content) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject(title);
+        message.setText(content);
+        javaMailSender.send(message);
+    }
+}
