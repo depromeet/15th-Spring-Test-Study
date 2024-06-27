10f8c7a15bfbe36ef3779e2336d3d97005b37cab
ybchar <uiurihappy@sju.ac.kr>
2024-06-27 10:14:40 +0900

    refactor: MailSender 의존성 역전 법칙 적용

---
+++ Yunbeom/src/main/java/com/depromeet/yunbeom/user/infrastructure/MailSenderImpl.java
@@ -0,0 +1,25 @@
+package com.depromeet.yunbeom.user.infrastructure;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Component;
+
+import com.depromeet.yunbeom.user.service.port.MailSender;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class MailSenderImpl implements MailSender {
+
+	private final JavaMailSender mailSender;
+
+	@Override
+	public void send(String email, String title, String content) {
+		SimpleMailMessage message = new SimpleMailMessage();
+		message.setTo(email);
+		message.setSubject(title);
+		message.setText(content);
+		mailSender.send(message);
+	}
+}
