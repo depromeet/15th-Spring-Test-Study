d8573bd503f18cce2d685b2611cfef93ac6e6bd4
nahyeon99 <nahyeonee99@gmail.com>
2024-06-27 21:14:46 +0900

    fix: git conflict 미반영 파일 처리

--- Nahyeon/src/main/java/com/depromeet/nahyeon/exception/CertificationCodeNotMatchedException.java
+++
@@ -1,8 +0,0 @@
-package com.depromeet.nahyeon.exception;
-
-public class CertificationCodeNotMatchedException extends RuntimeException {
-
-	public CertificationCodeNotMatchedException() {
-		super("자격 증명에 실패하였습니다.");
-	}
-}
--- Nahyeon/src/main/java/com/depromeet/nahyeon/repository/PostRepository.java
+++
@@ -1,6 +0,0 @@
-package com.depromeet.nahyeon.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<PostEntity, Long> {
-}
