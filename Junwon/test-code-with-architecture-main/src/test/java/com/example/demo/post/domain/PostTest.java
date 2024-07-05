e0038bc3a76df43151d08c497d253e4e7e1b1d35
wjdwnsdnjs13 <67488973+wjdwnsdnjs13@users.noreply.github.com>
2024-07-05 15:35:43 +0900

    스터디 3주차 중간 저장

--- Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/post/domain/PostTest.java
+++ Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/post/domain/PostTest.java
@@ -1,5 +1,41 @@
 package com.example.demo.post.domain;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class PostTest {
+
+    @Test
+    public void PostCreate로_게시물을_만들_수_있다() {
+//        given
+        PostCreate postCreate = PostCreate.builder()
+                .writerId(1)
+                .content("helloworld")
+                .build();
+        User writer = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .build();
+
+//        when
+        Post post = Post.from(writer, postCreate);
+
+//        then
+        assertThat(post.getContent()).isEqualTo("helloworld");
+        assertThat(post.getWriter().getEmail()).isEqualTo("kok202@naver.com");
+        assertThat(post.getWriter().getNickname()).isEqualTo("kok202");
+        assertThat(post.getWriter().getAddress()).isEqualTo("Seoul");
+        assertThat(post.getWriter().getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(post.getWriter().getCertificationCode()).isEqualTo("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab");
+    }
 
 }
--- Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/user/controller/response/MyProfileResponseTest.java
+++ Junwon/test-code-with-architecture-main/src/test/java/com/example/demo/user/controller/response/MyProfileResponseTest.java
@@ -1,4 +1,36 @@
 package com.example.demo.user.controller.response;
 
+import com.example.demo.user.domain.User;
+import com.example.demo.user.domain.UserStatus;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
 public class MyProfileResponseTest {
+
+    @Test
+    public void User로_응답을_생성할_수_있다() {
+//        given
+        User user = User.builder()
+                .id(1L)
+                .email("kok202@naver.com")
+                .nickname("kok202")
+                .address("Seoul")
+                .status(UserStatus.ACTIVE)
+                .lastLoginAt(100L)
+                .certificationCode("aaaaaaaa-aaaa-aaaa-aaaa-aaaaaaaaaaab")
+                .build();
+
+//        when
+        MyProfileResponse myProfileResponse = MyProfileResponse.from(user);
+
+//        then
+        assertThat(myProfileResponse.getId()).isEqualTo(1);
+        assertThat(myProfileResponse.getEmail()).isEqualTo("kok202@naver.com");
+        assertThat(myProfileResponse.getAddress()).isEqualTo("Seoul");
+        assertThat(myProfileResponse.getStatus()).isEqualTo(UserStatus.ACTIVE);
+        assertThat(myProfileResponse.getLastLoginAt()).isEqualTo(100L);
+
+    }
+
 }
